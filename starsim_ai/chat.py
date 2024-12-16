58b279a81555bda12dc43666ede6e0fdfba22b37
Katherine Rosenfeld <56368533+krosenfeld-IDM@users.noreply.github.com>
2024-12-16 06:03:31 -0800

    Update chat.py
    `'temperature'` will not be in kwargs

--- starsim_ai/chat.py
+++ starsim_ai/chat.py
@@ -17,9 +17,9 @@ class LLMModels(Enum):
 
 class BaseQuery():
     def __init__(self, model='gpt-3.5-turbo', temperature=0.0, **kwargs):
-
-        if 'temperature' not in kwargs:
-            kwargs.update({'temperature':temperature})
+        # Update sampling temperature
+        kwargs.update({'temperature':temperature})
+        
         # Validate and parse the configuration
         self.config = LLMConfig(model=model)
 
