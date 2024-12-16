ccf2f84bcaa197a0ecbf655412c599b7c7036de8
Katherine Rosenfeld <56368533+krosenfeld-IDM@users.noreply.github.com>
2024-12-16 05:46:50 -0800

    Update chat.py with lowered temperature
    Set default tempereature to 0 (instead of 0.7) (https://python.langchain.com/api_reference/openai/chat_models/langchain_openai.chat_models.base.ChatOpenAI.html#langchain_openai.chat_models.base.ChatOpenAI.temperature)

--- starsim_ai/chat.py
+++ starsim_ai/chat.py
@@ -16,7 +16,10 @@ class LLMModels(Enum):
 
 
 class BaseQuery():
-    def __init__(self, model='gpt-3.5-turbo', **kwargs):
+    def __init__(self, model='gpt-3.5-turbo', temperature=0.0, **kwargs):
+
+        if 'temperature' not in kwargs:
+            kwargs.update({'temperature':temperature})
         # Validate and parse the configuration
         self.config = LLMConfig(model=model)
 
