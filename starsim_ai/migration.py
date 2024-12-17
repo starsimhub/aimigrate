16f6b5099606b635816e46c8b82bab16ac02a4c3
Cliff Kerr <questionably@gmail.com>
2024-12-17 13:44:57 -0500

    fix include and exclude lists

--- starsim_ai/migration.py
+++ starsim_ai/migration.py
@@ -10,14 +10,15 @@ import starsim_ai as ssai
 __all__ = ['CodeFile', 'Migrate', 'migrate']
 
 # TODO: figure out how to expand the context to not need to exclude files
-default_include = [
-    "*.py",
-    "starsim/diseases/sir.py",
-    "starsim/diseases/hiv.py",
-    "starsim/diseases/gonorrhea.py",
-    "starsim/diseases/measles.py",
+default_include = ["*.py"]
+default_exclude = [
+    "docs/*",
+    "tests/*",
+    "__init__.py",
+    "setup.py",
+    'starsim/diseases/syphilis.py',
+    'starsim/diseases/cholera.py',
 ]
-default_exclude = ["docs/*", "tests/*", "__init__.py", "setup.py", 'starsim/diseases/syphilis.py']
 
 default_base_prompt = '''
 Here is the diff information for an update to the starsim (ss) package:
