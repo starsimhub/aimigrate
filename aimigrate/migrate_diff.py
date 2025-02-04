94021519fc6091a0b6c6599172f3fc933f1a85c8
Katherine Rosenfeld <56368533+krosenfeld-IDM@users.noreply.github.com>
2025-02-04 23:29:47 +0000

    cleanup

--- aimigrate/migrate_diff.py
+++ aimigrate/migrate_diff.py
@@ -9,23 +9,24 @@ import aimigrate as aim
 __all__ = ['MigrateDiff']
 
 # TODO: figure out how to expand the context to not need to exclude files
-default_include = ["*.py"]
-default_exclude = ["__init__.py", "setup.py"]
+DEFAULT_INCLUDE = ["*.py"]
+DEFAULT_EXCLUDE = ["__init__.py", "setup.py"]
 
-default_base_prompt = '''
-Here is the diff information for an update to the {library} ({library_alias}) library:
-<diff>
+DEFAULT_BASE_PROMPT = '''
+Here is the information for an update to the {library}{library_alias} library captured in a git diff:
+
+```
 {diff}
-</diff>
+```
 
-Please refactor the code below to maintain compatibility with the {library} library.
+Please update the code below to maintain compatibility with the {library} library.
 Maintain the same style, functionality, and structure as the original code.
 
-<code>
+```
 {code}
-</code>
+```
 
-Refactored code:
+Return your updated answer as a single code block embedded between three backticks (```).
 '''
 
 class MigrateDiff(aim.CoreMigrate):
@@ -43,14 +44,14 @@ class MigrateDiff(aim.CoreMigrate):
         self.library_alias = library_alias
         self.v_from = v_from
         self.v_to = v_to
-        self.include = sc.ifelse(include, default_include)
-        self.exclude = sc.ifelse(exclude, default_exclude)        
+        self.include = sc.ifelse(include, DEFAULT_INCLUDE)
+        self.exclude = sc.ifelse(exclude, DEFAULT_EXCLUDE)        
         self.diff_file = diff_file
         self.diff = diff
         self.patience = patience                
         self.model = model
         self.model_kw = sc.mergedicts(model_kw)
-        self.base_prompt = sc.ifelse(base_prompt, default_base_prompt)
+        self.base_prompt = sc.ifelse(base_prompt, DEFAULT_BASE_PROMPT)
         self.filter = sc.ifelse(filter, [".py"])
         self.parallel = parallel
         self.verbose = verbose
@@ -80,6 +81,9 @@ class MigrateDiff(aim.CoreMigrate):
             library_files = aim.files.get_python_files(self.library, gitignore=True, filter=self.filter)
             self.diff=''
             with aim.utils.TemporaryDirectoryChange(self.library):
+                # check that the revisions are good
+                assert not sc.runcommand(f"git rev-parse --verify {self.v_from}").startswith('fatal'), 'Invalid v_from'
+                assert not sc.runcommand(f"git rev-parse --verify {self.v_to}").startswith('fatal'), 'Invalid v_to'
                 for current_file in library_files:
                     if self.include and not any(fnmatch.fnmatch(current_file, pattern) for pattern in self.include):
                         continue
@@ -87,17 +91,6 @@ class MigrateDiff(aim.CoreMigrate):
                         continue
                     else:
                         self.diff += sc.runcommand(f"git diff {'--patience ' if self.patience else ''}{self.v_from} {self.v_to} -- {current_file}")
-
-    def parse_library(self):
-        """ Extract the right folder for library """
-        self.log('Parsing library folder')
-        if isinstance(self.library, types.ModuleType):
-            self.library = sc.thispath(self.library)
-        self.library = sc.path(self.library)
-        if not self.library.is_dir():
-            errormsg = f'The library must be supplied as the module or the folder path, not {self.library}'
-            raise FileNotFoundError(errormsg)
-        return
     
     def parse_diff(self):                   
         self.log('Parsing the diff')
@@ -112,7 +105,7 @@ class MigrateDiff(aim.CoreMigrate):
         for code_file in self.code_files:
             code_file.make_prompt(self.base_prompt,
                                   prompt_kwargs = {'library':self.library.stem,
-                                                   'library_alias':self.library_alias,
+                                                   'library_alias':f' ({self.library_alias })' if self.library_alias else '',
                                                     'diff':diff_string},
                                   encoder=self.encoder)
         return
