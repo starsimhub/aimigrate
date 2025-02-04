551fe20553cc74aa711de665dc8d1cd40f0c52a5
Katherine Rosenfeld <56368533+krosenfeld-IDM@users.noreply.github.com>
2025-02-04 22:14:56 +0000

    add docstring, handle optional alias

--- aimigrate/migrate_oob.py
+++ aimigrate/migrate_oob.py
@@ -3,23 +3,57 @@ Migrate out of the box
 """
 import aimigrate as aim
 import sciris as sc
+import types
 
 __all__ = ['MigrateOOB']
 
-default_base_prompt = """
-Refactor the code below to work with {library} ({library_alias}). Currently the 
+DEFAULT_BASE_PROMPT = """
+Update the code below to work with {library}{library_alias}. Currently the 
 code works with version {v_from} but needs to be updated to work with version {v_to}.
 Maintain the same style, functionality, and structure as the original code.
 
-<code>
+```
 {code}
-</code>
+```
 
-Refactored code:
+Return your updated answer as a single code block embedded between three backticks (```).
 """
 
 class MigrateOOB(aim.CoreMigrate):
+    """
+    Handle all steps of code migration using an LLM "out of the box".
 
+    Args:
+        source_dir (str/path): the source folder (or single file) to migrate
+        dest_dir (str/path): the destination folder to put the migrated files in
+        files (list): if provided, the list of files to migrate (else, migrate all Python files in the source folder)
+        library (str/module): the library to base the migration on
+        library_alias (str): string to use as the alias for the library
+        v_from (str): the git hash or version of Starsim that the code is currently written in
+        v_to (str): the git hash or version of Starsim that the new code should be written in
+        model (str): the LLM to use
+        model_kw (dict): any keywords to pass to the model
+        base_prompt (str): the prompt template that will be populated with the diff and file information
+        parallel (bool): whether to migrate the files in parallel
+        verbose (bool): print information during the migration (default True)
+        save (bool): whether to save the files to disk (default True)
+        run (bool): whether to perform the migration immediately (default False)
+
+    **Example**::
+
+        import aimigrate as aim
+
+        M = aim.MigrateOOB(
+            source_dir = '/path/to/your/code/folder', # folder with the code to migrate
+            dest_dir = '/path/to/migrated/folder', # folder to output migrated code into
+            library = ss, # can also be the name of the library as a string (e.g., starsim)
+            library_alias = 'ss', # the alias to use for the library in the prompt (optional)
+            v_from = 'v1.0', # can be any valid git tag or hash
+            v_to = 'v2.0', # ditto
+            model = 'gpt-4o', # see aim.Models for list of allowed models
+        )
+        M.run()
+    """
     def __init__(self, source_dir, dest_dir, files=None, # Input and output folders
                  library=None, library_alias=None, v_from=None, v_to=None, # Migration settings
                  model=None, model_kw=None, base_prompt=None, # Model settings
@@ -34,7 +68,7 @@ class MigrateOOB(aim.CoreMigrate):
         self.v_to = v_to        
         self.model = model
         self.model_kw = sc.mergedicts(model_kw)
-        self.base_prompt = sc.ifelse(base_prompt, default_base_prompt)
+        self.base_prompt = sc.ifelse(base_prompt, DEFAULT_BASE_PROMPT)
         self.parallel = parallel
         self.verbose = verbose
         self.save = save
@@ -46,6 +80,8 @@ class MigrateOOB(aim.CoreMigrate):
         self.code_files = []
         self.errors = []
 
+        assert isinstance(self.library, (str, types.ModuleType)), 'Library must be a string or module'
+
         # Optionally run
         if run:
             self.run()
@@ -55,8 +91,8 @@ class MigrateOOB(aim.CoreMigrate):
         self.make_encoder()
         for code_file in self.code_files:
             code_file.make_prompt(self.base_prompt, 
-                                  prompt_kwargs={'library': self.library, 
-                                                'library_alias': self.library_alias, 
+                                  prompt_kwargs={'library': self.library.__name__ if hasattr(self.library, '__name__') else self.library, 
+                                                'library_alias': f' ({self.library_alias })'if self.library_alias else '', 
                                                 'v_from': self.v_from,
                                                 'v_to': self.v_to},
                                                 encoder=self.encoder)
