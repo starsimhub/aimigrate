916686d92e35e53674aa160b442ec76ae24617b4
Katherine Rosenfeld <56368533+krosenfeld-IDM@users.noreply.github.com>
2025-02-05 00:10:06 +0000

    use MigrateDiff as default

--- aimigrate/__init__.py
+++ aimigrate/__init__.py
@@ -3,10 +3,10 @@ from .code import *
 from .embeddings import *
 from .files import *
 from .chat import *
-from .migration import *
 from .migrate_diff import *
 from .migrate_repo import *
 from .migrate_oob import *
+from .migration import *
 
 from . import paths
 from . import utils
