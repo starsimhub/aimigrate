a5514881dd9f3f208a4a995eebc16a11491d0771
Katherine Rosenfeld <56368533+krosenfeld-IDM@users.noreply.github.com>
2025-02-06 00:25:28 +0000

    cleanup

--- playpen/basic/simple_query.py
+++
@@ -1,8 +0,0 @@
-""" chat.py
-How to use SimpleQuery to query the LLM.
-"""
-import aimigrate as aim
-
-chatter = aim.SimpleQuery(model='meta-llama/Llama-3.2-1B')
-response = chatter("Hi, I'm in Oregon. What is the state to the North of me?")
-print(response)
