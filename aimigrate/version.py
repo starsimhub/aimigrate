334f05045ae51d60915553063b3f621210e14fc2
Katherine Rosenfeld <56368533+krosenfeld-IDM@users.noreply.github.com>
2025-02-06 17:40:49 +0000

    update for date as well

--- aimigrate/version.py
+++ aimigrate/version.py
@@ -5,5 +5,5 @@ Version and license information.
 __all__ = ["__version__", "__versiondate__", "__license__"]
 
 __version__ = "0.2.0"
-__versiondate__ = "2025-01-01"
+__versiondate__ = '2025-01-01'
 __license__ = "MIT"
