7452bf8708f954aee2dc251e6b2fce72746ca129
Katherine Rosenfeld <56368533+krosenfeld-IDM@users.noreply.github.com>
2025-02-06 17:24:24 +0000

    add version info

--- aimigrate/__init__.py
+++ aimigrate/__init__.py
@@ -1,4 +1,6 @@
 # ruff: noqa: F403, F401
+from .version import __version__, __versiondate__, __license__
+
 from .migrate_core import *
 from .code import *
 from .files import *
---
+++ aimigrate/version.py
@@ -0,0 +1,9 @@
+"""
+Version and license information.
+"""
+
+__all__ = ["__version__", "__versiondate__", "__license__"]
+
+__version__ = "0.1.0"
+__versiondate__ = "2025-01-01"
+__license__ = "MIT"
