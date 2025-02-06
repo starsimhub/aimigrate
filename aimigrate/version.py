8a970dfe72ab621110b5c61701bcfb3758c2e2bf
Katherine Rosenfeld <56368533+krosenfeld-IDM@users.noreply.github.com>
2025-02-06 17:41:38 +0000

    Bump version: 0.2.0 → 0.2.1

--- aimigrate/version.py
+++ aimigrate/version.py
@@ -4,6 +4,6 @@ Version and license information.
 
 __all__ = ["__version__", "__versiondate__", "__license__"]
 
-__version__ = "0.2.0"
-__versiondate__ = '2025-01-01'
+__version__ = "0.2.1"
+__versiondate__ = '2025-02-06'
 __license__ = "MIT"
